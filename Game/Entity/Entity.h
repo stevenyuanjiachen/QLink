eb3fef671f51db5de0e8172ca277651e9062cc30
Steven Yuan <1085320540@qq.com>
2024-08-08 16:32:22 +0800

    Entity lib - Add the Entity lib - Finish the Entity class

---
+++ Game/Entity/Entity.h
@@ -0,0 +1,24 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <QPainter>
+class Entity
+{
+public:
+    Entity();
+    virtual void update() = 0;
+    virtual void draw(QPainter* painter) = 0;
+
+    // the Acitve state of the Entity
+    inline bool isActive() { return this->active; }
+    inline void destroy() { this->active = false; }
+    
+    // the type of the Entity
+    inline int type() { return m_type; }
+    inline void set_type(int type) { this->m_type = type; }
+private:
+    bool active = true;
+    int m_type = 0;
+};
+
+#endif
