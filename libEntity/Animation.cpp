92f8d5f9a02727a905232c7d988e061f600f1ad3
Steven Yuan <1085320540@qq.com>
2024-08-18 20:13:41 +0800

    limit the hero's position

--- libEntity/Animation.cpp
+++ libEntity/Animation.cpp
@@ -1,4 +1,5 @@
 #include "Animation.h"
+#include "Map.h"
 
 Animation::Animation(const QStringList& image_list):
     index(0), animation_speed(6), counter(0)
@@ -24,6 +25,11 @@ void Animation::update()
 void Animation::draw(QPainter *painter, const QVector2D& postion)
 {
     painter->drawPixmap(postion.toPoint(), images[index]);
+    painter->drawPixmap(postion.x()-CUBE_LENGTH*MAP_WIDTH, postion.y(), images[index]); // left
+    painter->drawPixmap(postion.x()+CUBE_LENGTH*MAP_WIDTH, postion.y(), images[index]); // right
+    painter->drawPixmap(postion.x(), postion.y()-CUBE_LENGTH*MAP_HEIGHT, images[index]); // up
+    painter->drawPixmap(postion.x(), postion.y()+CUBE_LENGTH*MAP_HEIGHT, images[index]); // down
+
 }
 
 void Animation::restart()
--- libEntity/Hero.cpp
+++ libEntity/Hero.cpp
@@ -1,4 +1,5 @@
 #include "Hero.h"
+#include "Map.h"
 
 Hero::Hero(double x, double y)
 : Sprite(x, y, stand_down_list[0]), state(HS_stand_down)
@@ -18,6 +19,28 @@ void Hero::update()
 {
     Sprite::update();
     anima[state].update();
+    
+    // limit the position of the Hero
+    // out from left
+    if(position.x() < -pixmap.width())
+    {
+        position.setX(CUBE_LENGTH*MAP_WIDTH-pixmap.width());
+    }
+    // out from right
+    if(position.x() > CUBE_LENGTH*MAP_WIDTH)
+    {
+        position.setX(0);
+    }
+    // out from up
+    if(position.y() < -pixmap.height())
+    {
+        position.setY(CUBE_LENGTH*MAP_HEIGHT-pixmap.height());
+    }
+    // out from down
+    if(position.y() > CUBE_LENGTH*MAP_HEIGHT)
+    {
+        position.setY(0);
+    }
 }
 
 void Hero::draw(QPainter *painter)
