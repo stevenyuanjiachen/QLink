9822f27a614b96e792096dbdb773fc2cee2c197f
Steven Yuan <1085320540@qq.com>
2024-09-07 10:03:00 +0800

    ADJUST the res structure

--- libEntity/EntityResList.h
+++ libEntity/EntityResList.h
@@ -2,132 +2,132 @@
 #define ENTITY_RES_LIST
 
 // QLink Icon
-const QString GAME_ICON = "../images/Icon/QLink_icon.png";
+const QString GAME_ICON = "../res/res/icon/QLink_icon.png";
 
 // Hero Resource List
 const QStringList STAND_UP_LIST = 
 {
-    "../images/Player/stand_up1",
-    "../images/Player/stand_up2",
-    "../images/Player/stand_up3",
-    "../images/Player/stand_up4",
-    "../images/Player/stand_up5",
-    "../images/Player/stand_up6",
+    "../res/images/Player/stand_up1",
+    "../res/images/Player/stand_up2",
+    "../res/images/Player/stand_up3",
+    "../res/images/Player/stand_up4",
+    "../res/images/Player/stand_up5",
+    "../res/images/Player/stand_up6",
 };
 
 const QStringList STAND_DOWN_LIST = 
 {
-    "../images/Player/stand_down1",
-    "../images/Player/stand_down2",
-    "../images/Player/stand_down3",
-    "../images/Player/stand_down4",
-    "../images/Player/stand_down5",
-    "../images/Player/stand_down6",
+    "../res/images/Player/stand_down1",
+    "../res/images/Player/stand_down2",
+    "../res/images/Player/stand_down3",
+    "../res/images/Player/stand_down4",
+    "../res/images/Player/stand_down5",
+    "../res/images/Player/stand_down6",
 };
 
 const QStringList STAND_LEFT_LIST = 
 {
-    "../images/Player/stand_left1",
-    "../images/Player/stand_left2",
-    "../images/Player/stand_left3",
-    "../images/Player/stand_left4",
-    "../images/Player/stand_left5",
-    "../images/Player/stand_left6",
+    "../res/images/Player/stand_left1",
+    "../res/images/Player/stand_left2",
+    "../res/images/Player/stand_left3",
+    "../res/images/Player/stand_left4",
+    "../res/images/Player/stand_left5",
+    "../res/images/Player/stand_left6",
 };
 
 const QStringList STAND_RIGHT_LIST = 
 {
-    "../images/Player/stand_right1",
-    "../images/Player/stand_right2",
-    "../images/Player/stand_right3",
-    "../images/Player/stand_right4",
-    "../images/Player/stand_right5",
-    "../images/Player/stand_right6",
+    "../res/images/Player/stand_right1",
+    "../res/images/Player/stand_right2",
+    "../res/images/Player/stand_right3",
+    "../res/images/Player/stand_right4",
+    "../res/images/Player/stand_right5",
+    "../res/images/Player/stand_right6",
 };
 
 const QStringList MOVE_UP_LIST = 
 {
-    "../images/Player/move_up1",
-    "../images/Player/move_up2",
-    "../images/Player/move_up3",
-    "../images/Player/move_up4",
-    "../images/Player/move_up5",
-    "../images/Player/move_up6",
+    "../res/images/Player/move_up1",
+    "../res/images/Player/move_up2",
+    "../res/images/Player/move_up3",
+    "../res/images/Player/move_up4",
+    "../res/images/Player/move_up5",
+    "../res/images/Player/move_up6",
 };
 
 const QStringList MOVE_DOWN_LIST = 
 {
-    "../images/Player/move_down1",
-    "../images/Player/move_down2",
-    "../images/Player/move_down3",
-    "../images/Player/move_down4",
-    "../images/Player/move_down5",
-    "../images/Player/move_down6",
+    "../res/images/Player/move_down1",
+    "../res/images/Player/move_down2",
+    "../res/images/Player/move_down3",
+    "../res/images/Player/move_down4",
+    "../res/images/Player/move_down5",
+    "../res/images/Player/move_down6",
 };
 
 const QStringList MOVE_LEFT_LIST = 
 {
-    "../images/Player/move_left1",
-    "../images/Player/move_left2",
-    "../images/Player/move_left3",
-    "../images/Player/move_left4",
-    "../images/Player/move_left5",
-    "../images/Player/move_left6",
+    "../res/images/Player/move_left1",
+    "../res/images/Player/move_left2",
+    "../res/images/Player/move_left3",
+    "../res/images/Player/move_left4",
+    "../res/images/Player/move_left5",
+    "../res/images/Player/move_left6",
 };
 
 const QStringList MOVE_RIGHT_LIST = 
 {
-    "../images/Player/move_right1",
-    "../images/Player/move_right2",
-    "../images/Player/move_right3",
-    "../images/Player/move_right4",
-    "../images/Player/move_right5",
-    "../images/Player/move_right6",
+    "../res/images/Player/move_right1",
+    "../res/images/Player/move_right2",
+    "../res/images/Player/move_right3",
+    "../res/images/Player/move_right4",
+    "../res/images/Player/move_right5",
+    "../res/images/Player/move_right6",
 };
 
 // Map Resource List
 const QStringList MAP_LIST =
 {
-    "../images/Grass_Tile/0.png",
-    "../images/Grass_Tile/1.png",
-    "../images/Grass_Tile/2.png",
-    "../images/Grass_Tile/3.png",
-    "../images/Grass_Tile/4.png",
-    "../images/Grass_Tile/5.png",
-    "../images/Grass_Tile/6.png",
-    "../images/Grass_Tile/7.png",
-    "../images/Grass_Tile/8.png",
-    "../images/Grass_Tile/9.png"
+    "../res/images/Grass_Tile/0.png",
+    "../res/images/Grass_Tile/1.png",
+    "../res/images/Grass_Tile/2.png",
+    "../res/images/Grass_Tile/3.png",
+    "../res/images/Grass_Tile/4.png",
+    "../res/images/Grass_Tile/5.png",
+    "../res/images/Grass_Tile/6.png",
+    "../res/images/Grass_Tile/7.png",
+    "../res/images/Grass_Tile/8.png",
+    "../res/images/Grass_Tile/9.png"
 };
 
 // Box Resource List
 const QStringList BOX_RED_LIST =
 {
-    "../images/Box/box_red1.png",
-    "../images/Box/box_red2.png",
-    "../images/Box/box_red3.png",
-    "../images/Box/box_red4.png",
-    "../images/Box/box_red5.png",
-    "../images/Box/box_red6.png"
+    "../res/images/Box/box_red1.png",
+    "../res/images/Box/box_red2.png",
+    "../res/images/Box/box_red3.png",
+    "../res/images/Box/box_red4.png",
+    "../res/images/Box/box_red5.png",
+    "../res/images/Box/box_red6.png"
 };
 
 const QStringList BOX_BLUE_LIST =
 {
-    "../images/Box/box_blue1.png",
-    "../images/Box/box_blue2.png",
-    "../images/Box/box_blue3.png",
-    "../images/Box/box_blue4.png",
-    "../images/Box/box_blue5.png",
-    "../images/Box/box_blue6.png"
+    "../res/images/Box/box_blue1.png",
+    "../res/images/Box/box_blue2.png",
+    "../res/images/Box/box_blue3.png",
+    "../res/images/Box/box_blue4.png",
+    "../res/images/Box/box_blue5.png",
+    "../res/images/Box/box_blue6.png"
 };
 
 const QStringList BOX_PINK_LIST =
 {
-    "../images/Box/box_pink1.png",
-    "../images/Box/box_pink2.png",
-    "../images/Box/box_pink3.png",
-    "../images/Box/box_pink4.png",
-    "../images/Box/box_pink5.png",
-    "../images/Box/box_pink6.png"
+    "../res/images/Box/box_pink1.png",
+    "../res/images/Box/box_pink2.png",
+    "../res/images/Box/box_pink3.png",
+    "../res/images/Box/box_pink4.png",
+    "../res/images/Box/box_pink5.png",
+    "../res/images/Box/box_pink6.png"
 };
 #endif
